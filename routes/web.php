47f98b9f96c43927fc61f7589126ab2341bf5aa2
kamissvitoria <kmorssantos@gmail.com>
2025-09-16 15:25:37 -0300

    Registros

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\RegistroController;
 use App\Livewire\Dashboard;
+use App\Livewire\Registro\RegistroList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+Route::get('registro/list', RegistroList::class)->name('registro.list');
